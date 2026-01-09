11dc6b915b2a017661712428c55e7b58c66a510c
Huỳnh Phước Trung <th706020@gmail.com>
2026-01-09 10:51:31 +0700

    bai1

--- slot2/bai1.c
+++ slot2/bai1.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
+
 int main() {
-	// khai bao 1 bien so nguyen
-	int S;
-	//khoi tao gia tri bat ky
-	S = 10;
-	//in ra man hinh
-	printf("gia tri la: %d\n", S);
+    // khai bao 1 bien so nguyen
+    int S; //giai thich: khai bao bien S la gia tri so nguyen
+
+    //khoi tao gia tri bat ky
+    S = 10; //Giai thich: gan gia tri cho bien S la 10
+
+    //in ra man hinh
+    printf("gia tri la: %d\n", S); //giai thich: in ra man hinh bang lenh printf, va %d la chuoi dinh dang de gia tri dat vao dung vi tri do va la dinh dang so nguyen, va \n la xuong dong
+
 }
