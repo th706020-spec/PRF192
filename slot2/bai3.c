de86a3ce56fc363e460c352a684b99cefe3fe959
Huỳnh Phước Trung <th706020@gmail.com>
2026-01-09 11:33:43 +0700

    bai3

--- slot2/bai3.c
+++ slot2/bai3.c
@@ -5,13 +5,13 @@ int main() {
     float a = 5; //giai thich: khai bao bien a la gia tri so thuc (float or double) va co gia tri la 5
 	double b = 10.216; //giai thich: khai bao bien b la gia tri so thuc (float or double) va co gia tri la 10.216
     //khai bao bien kq, la ket qua cua a+b
-    float kq; //giai thich: khai bao bien kq la gia tri so thuc (float or double)
+    float kq = a + b ; //giai thich: khai bao bien kq la gia tri so thuc (float or double)
     //in a
     printf("a =  %.0f\n", a); //giai thich: in ra man hinh bang lenh printf, va %f la chuoi dinh dang de gia tri dat vao dung vi tri do va la dinh dang so thuc, va \n la xuong dong
     //in b
 	printf("b = %.3f\n", b); //giai thich: in ra man hinh bang lenh printf, va %f la chuoi dinh dang de gia tri dat vao dung vi tri do va la dinh dang so thuc, va \n la xuong dong
 	//in kq a+b
-	printf("5 + 10.216 = %.2f\n", a+b); //giai thich: in ra man hinh bang lenh printf, va %f la chuoi dinh dang de gia tri dat vao dung vi tri do va la dinh dang so thuc, va \n la xuong dong
+	printf("5 + 10.216 = %.2f\n", kq); //giai thich: in ra man hinh bang lenh printf, va %f la chuoi dinh dang de gia tri dat vao dung vi tri do va la dinh dang so thuc, va \n la xuong dong
 	
 	
 	
