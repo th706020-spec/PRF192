6864d1647c76f527632d7282f87bd0514bc5ddc8
Huỳnh Phước Trung <th706020@gmail.com>
2026-01-09 11:24:25 +0700

    bai3

---
+++ slot2/bai2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    // khai bao 1 bien so nguyen
+    float S; //giai thich: khai bao bien S la gia tri so thuc (float or double)
+
+    //khoi tao gia tri bat ky
+    S = 10.5; //Giai thich: gan gia tri cho bien S la 10.5
+
+    //in ra man hinh
+    printf("gia tri la: %f\n", S); //giai thich: in ra man hinh bang lenh printf, va %f la chuoi dinh dang de gia tri dat vao dung vi tri do va la dinh dang so thuc, va \n la xuong dong
+
+}
---
+++ slot2/bai3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main() {
+    // khai bao 2 bien so thuc, khai bao gia tri
+    float a = 5; //giai thich: khai bao bien a la gia tri so thuc (float or double) va co gia tri la 5
+	double b = 10.216; //giai thich: khai bao bien b la gia tri so thuc (float or double) va co gia tri la 10.216
+    //khai bao bien kq, la ket qua cua a+b
+    float kq; //giai thich: khai bao bien kq la gia tri so thuc (float or double)
+    //in a
+    printf("gia tri a la: %.0f\n", a); //giai thich: in ra man hinh bang lenh printf, va %f la chuoi dinh dang de gia tri dat vao dung vi tri do va la dinh dang so thuc, va \n la xuong dong
+    //in b
+	printf("gia tri b la: %.3f\n", b); //giai thich: in ra man hinh bang lenh printf, va %f la chuoi dinh dang de gia tri dat vao dung vi tri do va la dinh dang so thuc, va \n la xuong dong
+	//in kq a+b
+	printf("kq a+b la: %.2f\n", a+b); //giai thich: in ra man hinh bang lenh printf, va %f la chuoi dinh dang de gia tri dat vao dung vi tri do va la dinh dang so thuc, va \n la xuong dong
+	
+	
+	
+}
