5b8284eb4e56ab50c49685c2eb9bfe05f0a1bbaa
Huỳnh Phước Trung <th706020@gmail.com>
2026-01-14 08:30:56 +0700

    slot3

---
+++ slot3/ex1.c
@@ -0,0 +1,22 @@
+//1. khai bao a,b so nguyen
+//2. khai bao bien ket qua
+//hay in ket qua: +,-,*,/ cua a,b
+//vd:
+//a = 7
+//b = 2
+//7 + 2 = 9
+//7 - 2 = 5
+//7 * 2 = 14
+//7 / 2 = 3.50
+#include <stdio.h>
+
+int main(){
+	int a = 7;
+	int b = 2;
+	printf("a = %d\n", a);
+	printf("b = %d\n", b);
+	printf("a + b = %d\n", a + b );
+	printf("a - b = %d\n", a - b );
+	printf("a * b = %d\n", a * b );
+	printf("a / b = %.2f\n", (float) a/b );	
+}
---
+++ slot3/ex2.c
@@ -0,0 +1,13 @@
+//2. khai bao kieu ky tu kt, khoi gna gia tri cho bien nay
+//in ra gia tri cua bien
+//gia tri thap phan, gia tri hex, gia tri octal
+//kich thuoc kieu du lieu bien kt
+#include <stdio.h>
+
+int main(){
+	char kt = 'a';
+	printf("Gia tri cua bien: %c\n", kt);
+	printf("Gia tri Thap phan :%d\n", kt);
+    printf("Gia tri Hex :%x\n", kt);
+    printf("Gia tri Octal :%o\n", kt);
+}
