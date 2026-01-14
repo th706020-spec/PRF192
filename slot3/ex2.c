2ecc481422fe3690e55ba853782564f9bc2d313f
Huỳnh Phước Trung <th706020@gmail.com>
2026-01-14 09:28:19 +0700

    slot3

--- slot3/ex2.c
+++ slot3/ex2.c
@@ -5,7 +5,9 @@
 #include <stdio.h>
 
 int main(){
-	char kt = 'a';
-	printf("%-17s %-17s %-17s %-17s %-17s %-17s\n","ten bien","gia tri" ,"Dec", "Hex", "oct", "size");
-	printf("%-17c %-17d %-17d %-17x %-17o %-17d",kt,kt,kt ,kt , kt, sizeof(kt));
+	char kt = 'a',kt2= 'b',kt3='c';
+	printf("%-17s|%-17s|%-17s|%-17s|%-17s|%-17s\n","ten bien","gia tri" ,"Dec", "Hex", "oct", "size");
+	printf("%-17s|%-17d|%-17d|%-17x|%-17o|%-17d\n","kt",kt,kt ,kt , kt, sizeof(kt));
+	printf("%-17s|%-17d|%-17d|%-17x|%-17o|%-17d\n","kt2",kt2,kt2 ,kt2 , kt2, sizeof(kt2));
+	printf("%-17s|%-17d|%-17d|%-17x|%-17o|%-17d\n","kt3",kt3,kt3 ,kt3 , kt3, sizeof(kt3));
 }
