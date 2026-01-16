1b06e54c24115279d1cb3a9adb2902359a3ca12b
Huỳnh Phước Trung <th706020@gmail.com>
2026-01-16 11:31:50 +0700

    slot4

--- slot4/ex4.c
+++ slot4/ex4.c
@@ -7,9 +7,8 @@ int main(){
 	scanf("%d,%d",&a,&b);
 	printf("nhap phep toan:");
 	scanf(" %c",&pt);
-	if (b == 0){
+	if (b == 0)
 		printf ("khong the chia cho 0\n");
-	}
 	else {
 		kq = (float)a/b;
 		printf("ket qua: %d %c %d = %.2f",a,pt,b,kq);
