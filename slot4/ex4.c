14cdadaef4e7815ce747dc2fef490df79144a7b9
Huỳnh Phước Trung <th706020@gmail.com>
2026-01-16 11:46:36 +0700

    slot4

--- slot4/ex4.c
+++ slot4/ex4.c
@@ -6,11 +6,15 @@ int main(){
 	printf("nhap a,b: ");
 	scanf("%d,%d",&a,&b);
 	printf("nhap phep toan:");
-	scanf(" %c",&pt);
-	if (b == 0)
-		printf ("khong the chia cho 0\n");
-	else {
-		kq = (float)a/b;
-		printf("ket qua: %d %c %d = %.2f",a,pt,b,kq);
+	fflush(stdin);//co the them dau cach truoc %c de tranh lay ky tu enter hoac dung getchar()
+	scanf("%c",&pt);
+	if(pt == '/'){
+		if (b == 0)
+			printf ("khong the chia cho 0\n");
+		else {
+			printf("sai phep chia roi");
+			kq = (float)a/b;
+			printf("ket qua: %d %c %d = %.2f",a,pt,b,kq);
+		}
 	}
 }
