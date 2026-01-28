a26afee7cc0895ce1e658e7a18700a46c205182f
Huỳnh Phước Trung <th706020@gmail.com>
2026-01-28 08:59:26 +0700

    ws1

--- workshop1/ex5.c
+++ workshop1/ex5.c
@@ -2,7 +2,7 @@
 #include <math.h>
 
 int main(){
-	float km,p,tx,tn,pt,xc,kq;
+	float km,p,xc,kq;
 	printf("nhap so km di: ");
 	kq = scanf("%f", &km);
 	if (kq == 0 || km<0){
@@ -17,17 +17,17 @@ int main(){
 	}
 	if (km>=0){
 		if (km<=0.5){
-			p=12;
+			p=12000;
 		}
 		else if (km<=30){
-			p = 12 + ((km - 0.5) * 15);
+			p = 12000 + ((km - 0.5) * 15000);
 		}
 		else {
-			p = 12 + ((30-0.5) * 15) + ((km - 30) * 12);
+			p = 12000 + (29.5 * 15000) + ((km - 30) * 12000);
 		}
 		if (xc>5)
-			p = (xc - 5) +p;
+			p = (xc - 5)*1000 +p;
 	}
 	printf("|%-16s|%-16s|%-16s\n", "so km","so phut cho","tong tien");
-	printf("|%-16.3f|%-16.3f|%-16.3f", km,xc,p );
+	printf("|%-16.1f|%-16.0f|%-16.0f", km,xc,p );
 }
